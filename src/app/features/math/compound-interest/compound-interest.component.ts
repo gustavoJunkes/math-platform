23a03acf8bde30a5b803cc8b70438a7ed522d540
Gustavo Junkes <gujunkes50@gmail.com>
2022-07-25 19:36:51 -0300

    Implementing normal compound calculation.

--- src/app/features/math/compound-interest/compound-interest.component.ts
+++ src/app/features/math/compound-interest/compound-interest.component.ts
@@ -8,37 +8,59 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CompoundInterestComponent implements OnInit {
 
-  calculationForm!: FormGroup;
+  calculationForm1!: FormGroup;
+  calculationForm2!: FormGroup;
+    
+  calculationResult1!: number;
+  calculationResult2!: number;
+  
+  interestCalculationResult1!: number;
+  interestCalculationResult2!: number;
+
   showExplanation: boolean = false;
-  calculationResult!: number;
-  interestCalculationResult!: number;
-  keys!: string[];
-  constructor(private formBuilder: FormBuilder) { }
   resultObject!: any;
-  title: string = "Juros compostos"
+  title: string = "Cálculo de Juros"
+
+  constructor(private formBuilder: FormBuilder) { }
+  
 
   ngOnInit(): void {
-    this.calculationForm = this.formBuilder.group({
+    this.calculationForm1 = this.formBuilder.group({
       "Valor inicial": this.formBuilder.control('0', [
-        Validators.required
-      ]),
-      "Valor dos juros": this.formBuilder.control('0', [
         Validators.required
       ]),
       "Valor por período": this.formBuilder.control('0', [
         Validators.required
       ]),
+      "Valor dos juros": this.formBuilder.control('0', [
+        Validators.required
+      ]),      
       "Número de períodos": this.formBuilder.control('0', [
         Validators.required
+      ])
+    });
+    
+    this.calculationForm2 = this.formBuilder.group({
+      "Valor inicial": this.formBuilder.control('0', [
+        Validators.required
+      ]),     
+      "Valor por período": this.formBuilder.control('0', [
+        Validators.required
       ]),
+      "Valor dos juros": this.formBuilder.control('0', [
+        Validators.required
+      ]),
+      "Número de períodos": this.formBuilder.control('0', [
+        Validators.required
+      ])
     })    
   }
 
-  copy(value: number) {
+  copy(value: Object) {
     navigator.clipboard.writeText(value+"");
   }
 
-  calculate(form: FormGroup) {
+  calculate1(form: FormGroup) {
     var initialValue = Number(form.controls['Valor inicial'].value);
     var interestValue = Number(form.controls['Valor dos juros'].value);
     var numberPeriods = Number(form.controls['Valor por período'].value);
@@ -59,12 +81,24 @@ export class CompoundInterestComponent implements OnInit {
       console.log("------------------------------")
     }
     finalValue = +(finalValue.toFixed(2));
-    this.calculationResult = finalValue;
-    this.interestCalculationResult = totalInterest;
+    this.calculationResult1 = finalValue;
+    this.interestCalculationResult1 = totalInterest;
     console.log(finalValue)
   }
 
   changeShowExplanation() {
     this.showExplanation = !this.showExplanation;
+  }
+
+  calculate2(form: FormGroup) {
+    var initialValue = Number(form.controls['Valor inicial'].value);
+    var interestValue = Number(form.controls['Valor dos juros'].value);
+    var numberPeriods = Number(form.controls['Valor por período'].value);
+    var valuePeriod = Number(form.controls['Número de períodos'].value);
+    
+    var numericInterestValue = (interestValue / 100)*initialValue;
+    var finalValue = initialValue + numericInterestValue + valuePeriod * numberPeriods;
+    this.calculationResult2 = finalValue;    
+    this.interestCalculationResult2 = numericInterestValue;
   }
 }
