93f40ed52345de605f83d040a65f2203903524ae
Gustavo Junkes <gujunkes50@gmail.com>
2022-07-31 11:00:31 -0300

    Fixing factorial calculation.

--- src/app/features/math/factorial/factorial.component.ts
+++ src/app/features/math/factorial/factorial.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FactorialComponent implements OnInit {
 
-  constructor() { }
+  calculationForm1!: FormGroup;
+
+  calculationResult1!: number;
+
+  valueCopied!: Object;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  this.calculationForm1 = this.formBuilder.group({
+    "Fatorial": this.formBuilder.control('', [
+      Validators.required
+    ])
+  })
+  }
+
+  copy(value: number) {
+    navigator.clipboard.writeText(value+"");
+    this.valueCopied = value;
+  }
+
+  calculate1(form: FormGroup) {
+    var number = form.controls['Fatorial'].value;
+    this.calculationResult1 = this.calculateFactorial(number);
+  }
+
+  // 4! = 4*3*2*1
+  calculateFactorial(number:number): number {
+    if(number < 3 && number > -3) {
+      return number;
+    }
+    
+    var result = number;
+    for(let i = number - 1; i > 0; i--) {
+      result *= i;
+      //result *= this.calculateFactorial(i);
+    }
+
+    return result;
   }
 
 }
