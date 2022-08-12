5532cdc6d03293bcfb8f6218f0cfd80fbc5c3fc1
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-12 09:30:59 -0300

    Adding fourth percentage explanation

--- src/app/features/math/percentage/percentage.component.ts
+++ src/app/features/math/percentage/percentage.component.ts
@@ -21,7 +21,7 @@ export class PercentageComponent implements OnInit {
   showExplanation01: boolean = true;
   showExplanation02: boolean = true;
   showExplanation03: boolean = true;
-
+  showExplanation04: boolean = true;
 
   valueCopied!: Object;
 
