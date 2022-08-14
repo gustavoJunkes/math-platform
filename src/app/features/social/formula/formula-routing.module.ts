4ca060644dd4d202e4facdc8b212f57bbf961bc0
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-14 11:13:29 -0300

    Setting routing for new feature

--- src/app/features/social/formula/formula-routing.module.ts
+++ src/app/features/social/formula/formula-routing.module.ts
@@ -1,7 +1,22 @@
+import { MyFormulasComponent } from './my-formulas/my-formulas.component';
+import { NewFormulaComponent } from './new-formula/new-formula.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "nova-formula",
+    component: NewFormulaComponent
+  },
+  {
+    path: "",
+    redirectTo: "minhas-formulas"
+  },
+  {
+    path: "minhas-formulas",
+    component: MyFormulasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/features/social/social-routing.module.ts
+++ src/app/features/social/social-routing.module.ts
@@ -1,7 +1,21 @@
+import { FeedComponent } from './feed/feed.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "formula",
+    loadChildren: async () => import('./formula/formula.module').then((m) => m.FormulaModule)
+  },
+  {
+    path: "feed",
+    component: FeedComponent
+  },
+  {
+    path: "",
+    redirectTo: "feed"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
