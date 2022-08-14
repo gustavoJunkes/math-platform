39339580c36f4db3b1564cfc5d182764e1fe3d11
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-14 11:19:20 -0300

    Creating feed page

--- src/app/features/social/social.module.ts
+++ src/app/features/social/social.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -11,7 +12,8 @@ import { FeedComponent } from './feed/feed.component';
   ],
   imports: [
     CommonModule,
-    SocialRoutingModule
+    SocialRoutingModule,
+    SharedModule
   ]
 })
 export class SocialModule { }
