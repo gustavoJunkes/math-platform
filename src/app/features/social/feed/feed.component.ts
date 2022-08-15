c05f6c4ed85dcde8835499ed0fa4c2309f8ae397
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-15 11:15:58 -0300

    Adding button new formula

--- src/app/features/social/feed/feed.component.ts
+++ src/app/features/social/feed/feed.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Formula } from 'src/app/core/model/formula.model';
 import { Post } from 'src/app/core/model/post.model';
@@ -11,7 +12,7 @@ export class FeedComponent implements OnInit {
 
   posts!: Post[];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.posts = [
@@ -21,6 +22,10 @@ export class FeedComponent implements OnInit {
       new Post("Cálculo de taxa fob", "14/08/2022", new Formula("A = B + C", "Cálculo base para calcular salário de carteira assinada no Brasil segundo CLT. Fiz este cálculo para poder calcular meu próprio salário ao fim do mês. Espero que ajude alguêm :).", [])),
 
     ]
+  }
+
+  redirectToNewFormula() {
+    this.router.navigate(['social/formula/nova-formula'])
   }
 
 }
