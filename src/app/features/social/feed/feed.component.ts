52bb986fa366d99ee26afddf55f3fff2e5dd60d4
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-15 23:29:49 -0300

    Update feed component

--- src/app/features/social/feed/feed.component.ts
+++ src/app/features/social/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Formula } from 'src/app/core/model/formula.model';
+import { Post } from 'src/app/core/model/post.model';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  posts!: Post[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.posts = [
+      new Post("Cálculo de salário", "14/08/2022", new Formula("A = B + C", "Cálculo base para calcular salário de carteira assinada no Brasil segundo CLT. Fiz este cálculo para poder calcular meu próprio salário ao fim do mês. Espero que ajude alguêm :).", [])),
+      new Post("Cálculo de transportes", "14/08/2022", new Formula("A = B + C", "Cálculo base para calcular salário de carteira assinada no Brasil segundo CLT. Fiz este cálculo para poder calcular meu próprio salário ao fim do mês. Espero que ajude alguêm :).", [])),
+      new Post("Cálculo de juros baseado em...", "14/08/2022", new Formula("A = B + C", "Cálculo base para calcular salário de carteira assinada no Brasil segundo CLT. Fiz este cálculo para poder calcular meu próprio salário ao fim do mês. Espero que ajude alguêm :).", [])),
+      new Post("Cálculo de taxa fob", "14/08/2022", new Formula("A = B + C", "Cálculo base para calcular salário de carteira assinada no Brasil segundo CLT. Fiz este cálculo para poder calcular meu próprio salário ao fim do mês. Espero que ajude alguêm :).", [])),
+
+    ]
   }
 
 }
