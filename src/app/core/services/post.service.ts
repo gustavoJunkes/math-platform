5cdc372dc287282a1d05367b67b20288caa80863
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-18 10:49:45 -0300

    Creating post service

---
+++ src/app/core/services/post.service.ts
@@ -0,0 +1,21 @@
+import { Post } from './../model/post.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  baseUrl = 'http://localhost:8080/api';
+
+  constructor(private httpClient: HttpClient) { }
+
+  all() {
+    return this.httpClient.get<Post[]>(`${this.baseUrl}/post`);
+  }
+
+  save(post: Post) {
+    return this.httpClient.post<Post>(`${this.baseUrl}/post`, post);
+  }
+}
