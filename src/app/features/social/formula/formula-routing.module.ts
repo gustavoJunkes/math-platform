701cc2ff07ee6210a5d731597953238ce172f030
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-25 10:42:14 -0300

    Update formula-routing.module.ts

--- src/app/features/social/formula/formula-routing.module.ts
+++ src/app/features/social/formula/formula-routing.module.ts
@@ -1,7 +1,20 @@
+import { Post } from 'src/app/core/model/post.model';
+import { PostService } from './../../../core/services/post.service';
 import { MyFormulasComponent } from './my-formulas/my-formulas.component';
 import { NewFormulaComponent } from './new-formula/new-formula.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterModule, Routes } from '@angular/router';
+import { FormulaCalculationComponent } from './formula-calculation/formula-calculation.component';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class PostDataResolver implements Resolve<Post> {
+  constructor(private postService: PostService) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<Post> {
+    return this.postService.byId(route.params['id']);
+  }
+}
 
 const routes: Routes = [
   {
@@ -15,11 +28,21 @@ const routes: Routes = [
   {
     path: "minhas-formulas",
     component: MyFormulasComponent
+  },
+  {
+    path: ":id",
+    component: FormulaCalculationComponent,
+    resolve: {
+      post: PostDataResolver
+    }
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    PostDataResolver
+  ]
 })
 export class FormulaRoutingModule { }
