f0ffcd1be134ff4d7cc5c99c3c0952d20b6ecd5e
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-31 13:40:28 -0300

    Update formula.service.ts

--- src/app/core/services/formula.service.ts
+++ src/app/core/services/formula.service.ts
@@ -1,13 +1,14 @@
 import { Formula } from './../model/formula.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Variable } from '../model/variable.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormulaService {
 
-  baseUrl = 'http://localhost:8080/api';
+  baseUrl = 'http://localhost:8080/api/formula';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -17,6 +18,10 @@ export class FormulaService {
 
   save(formula: Formula) {
     return this.httpClient.post<Formula>(`${this.baseUrl}`, formula);
+  }
+
+  calculate(id: string, values: object) {    
+    return this.httpClient.post<String>(`${this.baseUrl}/calculate?formulaId=${id}`, values)
   }
 
 }
