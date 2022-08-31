83830a2460d24e78a6f64741a21d586c3cbf7d7e
Gustavo Junkes <gujunkes50@gmail.com>
2022-08-31 13:40:49 -0300

    Update dynamic-form.component.ts

--- src/app/shared/dynamic-form/dynamic-form.component.ts
+++ src/app/shared/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { QuestionBase } from 'src/app/core/model/question-base.model';
 import { QuestionControlService } from 'src/app/core/services/question-control.service';
@@ -7,19 +7,33 @@ import { QuestionControlService } from 'src/app/core/services/question-control.s
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  providers: [ QuestionControlService ]
+  providers: [QuestionControlService]
 })
 export class DynamicFormComponent implements OnInit {
 
-  @Input() questions: QuestionBase<string>[] | null = [];
+  @Input() questions: QuestionBase<string>[] = [];
   form!: FormGroup;
 
+  @Output()
+  resultObject = new EventEmitter();
 
-  constructor(private qcs: QuestionControlService) {}
+  constructor(private qcs: QuestionControlService) { }
 
   ngOnInit() {
     console.log("aham: " + this.questions)
-    this.form = this.qcs.toFormGroup(this.questions as QuestionBase<string>[]);
+    if(this.questions != null) {
+      console.log(this.questions != null)
+      this.form = this.qcs.toFormGroup(this.questions);  
+    }
+    
+  }
+
+  onSubmit() {
+    this.questions?.forEach(question => {
+      console.log(question.key + " - " + this.form.controls[question.key].value)
+    })
+    console.log(this.form.controls["a"])
+    this.resultObject.emit()
   }
 
 }
