c4ffa017212b4b08063585e391bec433cdb00ca8
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-22 18:48:55 -0300

    Add sidebar to percentage page and fixing header content in smaller screens

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeaturesRoutingModule } from './features-routing.module';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FeaturesRoutingModule
+    FeaturesRoutingModule,
+    MatSidenavModule,
+    SharedModule
   ]
 })
 export class FeaturesModule { }
